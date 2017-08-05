4194ecc44b20862ae106f0ef8681be5b446c2e00
Benjamin Fuchs <email@benjaminfuchs.de>
2017-08-05 23:44:43 +0200

    [JENKINS-41284] Implementing a custom anchor to give it an offset.

--- src/main/resources/org/jenkinsci/plugins/linenumbers/LineNumbersAnnotatorFactory/script.js
+++ src/main/resources/org/jenkinsci/plugins/linenumbers/LineNumbersAnnotatorFactory/script.js
@@ -1,5 +1,32 @@
-Behaviour.specify('div.fold', 'fold.toggle', 10, function(element) {
-	element.onclick = function() {
-		element.toggleClassName('open');
-	}
-});
+/*global window */
+(function () {
+    "use strict";
+
+    Behaviour.specify('div.fold', 'fold.toggle', 10, function (element) {
+        element.onclick = function () {
+            element.toggleClassName('open');
+        };
+    });
+
+    function scrollToElement(element) {
+        window.scrollTo(0, element.offsetTop - 30);
+    }
+
+    $(document).on("click", "a[href^='#L']", function (event) {
+        event.preventDefault();
+        scrollToElement(event.target);
+    });
+
+    $(document).observe("dom:loaded", function () {
+        var hash = window.location.hash.substring(1),
+            targetLine;
+        if (hash.length !== 0) {
+            targetLine = document.getElementById(hash);
+            if (targetLine) {
+                window.setTimeout(function () {
+                    scrollToElement(targetLine);
+                }, 200);
+            }
+        }
+    });
+}());
